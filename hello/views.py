1c3935398879b82fd76d0809199df562dc287e75
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-12-05 10:28:40 +0900

    2-2-2views.pyで実行すること

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# HttpResponseというクラスをimportする。
 
+
+def index(request):
+    return HttpResponse("Hello Django!!")
+#importされたクラスを実行する処理の内容
+#def index(request): index関数を定義しますよという意味
+#return HttpResponse〜で実行内容
+#
 # Create your views here.
