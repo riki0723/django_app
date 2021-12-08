281b0c541006df5d42387b2126403fa00c8755db
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-12-08 22:59:32 +0900

    2-1-5　クエリパラメーターの表示

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello Django!!")
+    msg = request.GET['msg']
+    return HttpResponse('you typed: "'+ msg +'".')
 #importされたクラスを実行する処理の内容
 #def index(request): index関数を定義しますよという意味
 #return HttpResponse〜で実行内容
