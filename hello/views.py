17f707c52ae324491663ac9957da55ca62d02338
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-12-08 23:50:03 +0900

    2-1-6　スマートな値の送り方

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 # HttpResponseというクラスをimportする。
 
 
-def index(request):
-    msg = request.GET['msg']
-    return HttpResponse('you typed: "'+ msg +'".')
+def index(request,id,nickname):
+    result = 'your id: ' + str(id) + ', name:"' + nickname + '".'
+    return HttpResponse(result)
 #importされたクラスを実行する処理の内容
 #def index(request): index関数を定義しますよという意味
 #return HttpResponse〜で実行内容
