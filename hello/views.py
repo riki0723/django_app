caded26d5e187b9d0aa3c0485cb2610b7c2ce867
riki0723 <69281517+riki0723@users.noreply.github.com>
2021-12-22 01:11:33 +0900

    2-2-3 テンプレートの表示

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 # HttpResponseというクラスをimportする。
 
 
-def index(request,nickname, age):
-    result = 'your account: ' + nickname + '"(' + str(age) + ').'
-    return HttpResponse(result)
+def index(request):
+    return render(request, 'hello/index.html')
+
 #importされたクラスを実行する処理の内容
 #def index(request): index関数を定義しますよという意味
 #return HttpResponse〜で実行内容
