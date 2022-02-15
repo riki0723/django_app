5eed53cdd520d85ef9f02fa5cd84fc046f575737
riki0723 <69281517+riki0723@users.noreply.github.com>
2022-02-15 22:42:20 +0900

    2-2-5

--- hello/urls.py
+++ hello/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('next', views.next, name='next'),
+  path('',views.index,name = 'index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HelloForm
 # HttpResponseというクラスをimportする。
 
 
 def index(request):
     params = {
-        'title':'Hello/Index',
-        'msg':'これは、サンプルで作ったページです。',
-        'goto':'next',
+        'title':'Hello',
+        'msg':'your data:',
+        'form': HelloForm()
     }
-    return render(request, 'hello/index.html',params)
-
-def next(request):
-    params = {
-        'title':'Hello/Next',
-        'msg':'これは、もう1つのページです。',
-        'goto':'index',
-    }
+    if (request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name'] + '<br>メール：' + request.POST['mail'] + '<br>年齢：' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
     return render(request, 'hello/index.html',params)
 
 #importされたクラスを実行する処理の内容
